fd7d1415aae6ba055d720c9e4de0a06132054a1c
Talvanes Ferreira de Sousa <talvanes_sousa@hotmail.com>
2020-06-03 13:24:04 -0300

    react-hyperscript: Refactor code to "hyper" syntax

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 import { hot } from "react-hot-loader";
 import "./App.css";
+import h from "react-hyperscript";
 
 import img from './assets/images/image.png';
 import svg from './assets/images/image.svg';
 import audio from './assets/audios/audio.mp3';
 import video from './assets/videos/video.mp4';
 
+
 class App extends Component {
     render() {
-        return (
-            <div className="App">
-                <h1>Hello, World!</h1>
-                <img src={img} alt="React Starter" title="React Starter" height="500"/>
-                <img src={svg} alt="React Starter" title="React Starter" height="500"/>
-                <audio src={audio} controls></audio>
-                <video src={video} controls></video>
-            </div>
-        );
+        return h('div', {className: 'App'}, [
+            h('h1', "Hello, World!"),
+            h('img', { src: img, alt: "React Starter", title: "React Starter", height: 500 }),
+            h('img', { src: svg, alt: "React Starter", title: "React Starter", height: 500 }),
+            h('audio', { src: audio, controls: true }),
+            h('video', { src: video, controls: true })
+        ]);
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
+import h from "react-hyperscript";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(h(App), document.getElementById('root'));
